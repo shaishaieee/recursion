c9014de5c30c6628467708748df7f9c7d7fe48ee
shaishaieee <bughawshairamae11@gmail.com>
2025-03-12 10:50:43 +0800

    first commit

---
+++ recursion.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    if n <=0:
+        return "Invalid Output"
+    elif n ==1:
+        return 0
+    elif n == 2:
+        return 1
+
+    return fibonacci(n-1) + fibonacci(n-2)
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(fibonacci(8))
---
+++ tree.py
@@ -0,0 +1,42 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+#In-Order
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.value, end=" ")
+        inorder_traversal(root.right)
+
+# Pre-Order
+def preorder_traversal(root):
+    if root:
+        print(root.value, end=" ")
+        preorder_traversal(root.left)
+        preorder_traversal(root.right)
+
+#Post Order
+def postorder_traversal(root):
+    if root:
+        postorder_traversal(root.left)
+        postorder_traversal(root.right)
+        print(root.value, end=" ")
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.right = TreeNode(5)
+root.left.left = TreeNode(4)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+print("In-order Traversal")
+inorder_traversal(root)
+print("\nPre-order Traversal")
+preorder_traversal(root)
+print("\nPost Order Traversal")
+postorder_traversal(root)

334f15005983605bfb829279ab45ef4f95b5c791
shaishaieee <bughawshairamae11@gmail.com>
2025-03-12 12:19:23 +0800

    up

--- linklist.py
+++ linklist.py
@@ -16,10 +16,11 @@ class Linklist:
 
         last = self.head
         while last.next:
-            last.next = new_node
+            last = last.next
+        last.next = new_node
 
     def print_recursion(self, node):
-        if node is Node:
+        if node is None:
             return
         print(node.data)
         self.print_recursion(node.next)
@@ -27,13 +28,11 @@ class Linklist:
     def start_recursion_traversal(self):
         self.print_recursion(self.head)
 
-if __name__ == "__main__":
-    linklist = Linklist()
+linklist = Linklist()
+linklist.insert(10)
+linklist.insert(20)
+linklist.insert(30)
+linklist.insert(40)
 
-    linklist.insert(10)
-    linklist.insert(20)
-    linklist.insert(30)
-    linklist.insert(40)
-
-    print("Linked List")
-    linklist.start_recursion_traversal()
+print("Linked List")
+linklist.start_recursion_traversal()
--- recursion.py
+++ recursion.py
@@ -8,10 +8,5 @@ def fibonacci(n):
 
     return fibonacci(n-1) + fibonacci(n-2)
 
-
-
-
-
-
 if __name__ == "__main__":
     print(fibonacci(8))
